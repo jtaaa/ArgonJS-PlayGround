eb32739cdfa85e4f96d1abcc9855338acc96139b
joshuatallum@gmail.com <joshuatallum@gmail.com>
2017-08-07 14:52:41 -0400

    Add endpoints to change ArgonJS environment

--- server.js
+++ server.js
@@ -6,15 +6,20 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
+app.get('/', function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
@@ -22,13 +27,32 @@ io.on('connection', function(socket){
   console.log('a user connected');
 });
 
-app.get("/colorchange/:entityId/:color", (req, res) => {
+app.get('/colorchange/:entityId/:color', (req, res) => {
   console.log(`The entity selected is: ${req.params.entityId}`);
   console.log(`The color selected is: ${req.params.color}`);
   io.emit('color change', req.params.entityId, req.params.color);
+  res.end('Done!');
 });
 
-app.get("/dreams", function (request, response) {
+app.post('/sizechange/:entityId', (req, res) => {
+  console.dir(req.body);
+  io.emit('size change', req.params.entityId, req.body.sizes);
+  res.end('Done!');
+});
+
+app.post('/positionchange/:entityId', (req, res) => {
+  console.dir(req.body);
+  io.emit('position change', req.params.entityId, req.body.position);
+  res.end('Done!');
+});
+
+app.post('/addentity', (req, res) => {
+  console.dir(req.body);
+  io.emit('entity add', req.body.entityData);
+  res.end('Done!');
+});
+
+app.get('/dreams', function (request, response) {
   response.send(dreams);
 });
 
@@ -46,6 +70,6 @@ var dreams = [
 ];
 
 // listen for requests :)
-var listener = http.listen(process.env.PORT, function () {
+var listener = http.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
